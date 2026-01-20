a7dcc2c6b90518f1a7445d47cdec945a4779234a
Seroga <ilchuk.cergey@gmail.com>
2026-01-20 12:09:02 +0200

    fixed flake8 errors

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 class Animal:
     alive = []
+
     def __init__(self, name: str, health: int = 100) -> None:
         self.name = name
         self.health = health
@@ -13,9 +14,11 @@ class Animal:
             f"Hidden: {self.hidden}}}"
         )
 
+
 class Herbivore(Animal):
     def hide(self) -> None:
         self.hidden = not self.hidden
+
 
 class Carnivore(Animal):
     @staticmethod
